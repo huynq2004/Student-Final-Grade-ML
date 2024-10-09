18859990527f0ce26d5cbb119a044466e49e7f40
huynq2004 <huynguyenfw@gmail.com>
2024-10-09 13:10:10 +0700

    thêm các hàm xử lý dữ liệu đầu vào

---
+++ src/ridge-regression/ridge_utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+# Hàm đọc dữ liệu từ file CSV
+def load_fold_data(fold_number):
+    """Đọc dữ liệu của một fold từ file CSV."""
+    train_data = pd.read_csv(f'data/split/K-folds/fold_{fold_number}_train.csv')
+    val_data = pd.read_csv(f'data/split/K-folds/fold_{fold_number}_val.csv')
+
+    # Tách biến đầu vào (cw1, mid-term, cw2) và biến đầu ra (final)
+    X_train = train_data[['cw1', 'mid-term', 'cw2']].values
+    y_train = train_data['final'].values
+
+    X_val = val_data[['cw1', 'mid-term', 'cw2']].values
+    y_val = val_data['final'].values
+
+    return X_train, y_train, X_val, y_val
+
+def load_data_from(path):
+    input = pd.read_csv(f'{path}')
+    data = input[['cw1', 'mid-term', 'cw2']].values
+    
+    return data
