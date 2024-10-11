a42248a887f4d54acf35801551cc0f08529d0770
Nguyen Huy <68539896+huynq2004@users.noreply.github.com>
2024-10-11 16:31:40 +0700

    Update run-ridge-model.py

--- src/ridge-regression/run-ridge-model.py
+++ src/ridge-regression/run-ridge-model.py
@@ -8,9 +8,9 @@ import ridge_plots as plot
 lamda_values = np.logspace(-2, 1, num=100)
 
 # 2. Huấn luyện mô hình và tìm giá trị λ tối ưu
-best_lamda, avg_errors = model.train_on_folds(fold_count=5, lamda_values=lamda_values)#lưu 2 cái gt này
+best_lamda, avg_errors = model.train_on_folds(fold_count=5, lamda_values=lamda_values)
 
-print(f'GT lamda best: {best_lamda}')
+print(f'Giá trị λ tối ưu: {best_lamda}')
 
 # 3. Lưu trữ mô hình với λ tốt nhất
 
@@ -22,17 +22,18 @@ np.save('ridge_model_weights.npy', w)
 
 # 4. Dự đoán trên tập test
 # Đọc dữ liệu test
-X_test, y_test = utils.load_data_from('D:/Project_ML/Student-Final-Grade-ML/data/split/test_data.csv')
+X_test, y_test = utils.load_data_from('data/split/test_data.csv')
+
 # Dự đoán
-weights = np.load('ridge_model_weights.npy')  # Tải trọng số từ file, gọi hàm thôi
+weights = np.load('ridge_model_weights.npy')  # Tải trọng số từ file
 y_pred = model.predict(X_test, weights)
 
 # 5. Tính lỗi và vẽ biểu đồ
 mse_test = np.mean((y_test - y_pred) ** 2)
-print(f'Error mean test: {mse_test}')
+print(f'Lỗi trung bình trên tập test: {mse_test}')
 
 # Vẽ biểu đồ lỗi theo các giá trị λ
 plot.plot_errors(lamda_values, avg_errors)
 
 # Vẽ biểu đồ so sánh điểm final dự đoán và thực tế trên tập test
-plot.plot_final_scores_comparison('D:/Project_ML/Student-Final-Grade-ML/data/split/test_data.csv', 'ridge_model_weights.npy')
+plot.plot_final_scores_comparison('data/split/test_data.csv', 'ridge_model_weights.npy')
